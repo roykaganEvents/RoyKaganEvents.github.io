6d1e0261f3540b6e4cce5a286191cd4eac5bd9bc
Roy Kagan <kagan.roy.mySite@gmail.com>
2020-04-27 15:23:33 +0300

    php file save

--- getInput.php
+++ getInput.php
@@ -3,14 +3,12 @@ if (isset($_POST['submit'])){
 	$firstname = $_POST['firstname'];
 	$lastname = $_POST['lastname'];
 	$peoplenum= $_POST['peoplenum'];
-	$yes= $_POST['yes'];
-	$no = $_POST['no'];
 	$email = $_POST['email'];
 
 
 	$mailTo = "kagan.roy.mySite@gmail.com";
 	$headers ="From: ".$email;
-	$txt = "קיבלת אימייל מ".$firstname.$lastname.".\n\n".$peoplenum.$yes.$no;
+	$txt = "קיבלת אימייל מ".$firstname.$lastname.".\n\n".$peoplenum;
 	
 	mail($mailTo,$txt,$headers);
 
