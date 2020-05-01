d8bf24746995a7370de85d692f8d7cc1469a4a72
roy kagan <kagan.roy.mySite@gmail.com>
2020-05-01 15:56:07 +0300

    example

---
+++ example/script.js
@@ -0,0 +1,12 @@
+function loadcss(filename){
+    var fileref= document.createElement("link");
+    fileref.setAttribute("rel", "stylesheet");
+    fileref.setAttribute("type", "text/css");
+    fileref.setAttribute("href", filename);
+
+    if (typeof fileref!="undefined"){
+        document.getElementById("head").appendChild(fileref);
+    }
+}
+
+loadcss("themes/green.css");
