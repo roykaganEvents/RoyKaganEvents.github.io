14615ad6316cb7e6b0142381983fcf1da68bae45
roy kagan <kagan.roy.mySite@gmail.com>
2020-05-11 19:02:58 +0300

    only password_corona

--- shelanu/password.js
+++ shelanu/password.js
@@ -1,5 +1,5 @@
 var pass45 = null;
-var pass1="123";
+var pass1="shelanu";
 alert('חכה/י לטעינת האתר');
 password();
 function password(){
@@ -10,6 +10,7 @@ function password(){
         if (pass45==pass1){
             alert('הקש\י על כפתור הOK לפתיחת האתר');
             x=10;
+            window.location="file:///C:/Users/%D7%9E%D7%A9%D7%AA%D7%9E%D7%A9/Desktop/RoyKaganEvents.github.io/shelanu/main%20copy.html";
         }
         else{
             
